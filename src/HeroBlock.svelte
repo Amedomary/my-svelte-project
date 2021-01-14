a89c468c8ff605a637d3d882534410ae1298c300
Maksim Grishin <maksim.grishin@simbirsoft.com>
2021-01-14 16:08:18 +0400

    start

---
+++ src/HeroBlock.svelte
@@ -0,0 +1,43 @@
+<script>
+  export let head;
+</script>
+
+
+<h2 class='logo'>{head}</h2>
+
+<section>
+	<h1>Все Тренеры и Корты по теннису в одном приложении</h1>
+	<button>Скачать 1</button>
+	<button>Скачать 2</button>
+</section>
+
+<style>
+	.logo {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	section {
+		border-radius: 16px;
+		max-width: 980px;
+		background-color: yellowgreen;
+		margin: auto;
+		padding: 32px;
+	}
+  /* main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	} */
+</style>
