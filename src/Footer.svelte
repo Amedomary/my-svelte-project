e456fca806eaf3981c8c230fc7205909c773cbca
Maksim Grishin <maksim.grishin@simbirsoft.com>
2021-01-14 16:12:17 +0400

    add footer

---
+++ src/Footer.svelte
@@ -0,0 +1,41 @@
+<script>
+  // export let head;
+</script>
+
+<!-- <h2 class="logo">{head}</h2> -->
+
+<footer>
+  <p>Есть вопросы? Напишите нам в Телеграм или на support@travers.app©</p>
+  <p class='copyright'>2020, TraversОфертаДетали оплаты и реквизиты</p>
+</footer>
+
+<style>
+  .copyright {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 1em;
+    font-weight: 100;
+  }
+
+  footer {
+    border-radius: 16px;
+    max-width: 980px;
+    /* background-color: yellowgreen; */
+    margin: auto;
+    padding: 32px;
+  }
+  /* main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	} */
+</style>
--- src/HeroBlock.svelte
+++ src/HeroBlock.svelte
@@ -26,18 +26,4 @@
 		margin: auto;
 		padding: 32px;
 	}
-  /* main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	} */
 </style>
